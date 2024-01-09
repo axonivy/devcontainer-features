2b4a9948a9917b6c91d8519eab875c68ef338167
Louis <louis.mueller@axonivy.com>
2024-01-09 10:47:58 +0100

    init engine feature (#1)

---
+++ src/engine/install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+apt_get_update() {
+  if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+    echo "Running apt-get update..."
+    apt-get update -y
+  fi
+}
+
+check_packages() {
+  if ! dpkg -s "$@" > /dev/null 2>&1; then
+    apt_get_update
+    apt-get -y install --no-install-recommends "$@"
+  fi
+}
+
+check_packages wget ca-certificates unzip
+
+echo "Downloading Axon Ivy Engine from '$DOWNLOADURL'"
+wget ${DOWNLOADURL} -O /tmp/ivy.zip --no-verbose
+
+echo "Unziping Axon Ivy Engine to '$ENGINEHOME'"
+unzip /tmp/ivy.zip -d ${ENGINEHOME}
+rm -f /tmp/ivy.zip
+
+mkdir ${ENGINEHOME}/applications
+mkdir ${ENGINEHOME}/configuration/applications && \
+chown -R ${_REMOTE_USER}:0 ${ENGINEHOME} && \
+chmod -R g=u ${ENGINEHOME}
